a6f25c1d4aaaff8027c0be860988916ab2e3a453
monycka <monyckadc@gmail.com>
2020-10-22 21:37:57 -0700

    Updated footer links

--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,10 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/monycka/weather-app" target="_blank">
+      <a href="https://github.com/monycka/weather-app" target="_blank" rel="noopener noreferrer">
         Open-source code
       </a>{" "}
-      by <a href="https://www.linkedin.com/in/monyckadc/" target="_blank">Monica Colorado</a>
+      by <a href="https://www.linkedin.com/in/monyckadc/" target="_blank" rel="noopener noreferrer">Monica Colorado</a>
     </div>
   );
 }
