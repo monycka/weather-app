c62a802c17d730298e91c6fc658626dcf1feeaac
monycka <monyckadc@gmail.com>
2020-10-22 20:39:55 -0700

    Updated the width of the container

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      
       <div className="container">
         <Container />
         <h1 className="Header"> Weather App</h1>
