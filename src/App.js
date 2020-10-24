72ddb9153a402d6203041b1c8c4846e51c361084
monycka <monyckadc@gmail.com>
2020-10-23 22:15:50 -0700

    Deleted a container

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import Weather from "./Weather";
 import Forecast from "./Forecast";
 import Footer from "./Footer";
-
-import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
@@ -12,7 +10,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Container />
         <h1 className="Header"> Weather App</h1>
         <Weather defaultCity="San Francisco" />
         <Forecast />
